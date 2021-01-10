ca5e2b8ecbe7b492680542b29c66e1e2c0244dcf
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-01-10 15:53:38 -0600

    output tagged template calls for flat arrays

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,7 @@
 export function raise(msg: string): never {
   throw new Error(msg)
 }
+
+export function isTruthy<T>(value: T): value is Exclude<T, undefined | null | false | 0 | ''> {
+  return Boolean(value)
+}
--- src/macro.ts
+++ src/macro.ts
@@ -1,6 +1,11 @@
 import { types } from '@babel/core'
 import { createMacro, MacroParams } from 'babel-plugin-macros'
+import { isTruthy } from './helpers'
 import { findJsxAttributeByName, getJsxAttributeName, getJsxAttributeValue } from './jsx-attribute'
+
+function onlyContainsExpressions(nodes: types.Node[]): nodes is types.Expression[] {
+  return nodes.every((node) => types.isExpression(node))
+}
 
 function createTwCall(value: types.Expression) {
   if (types.isStringLiteral(value)) {
@@ -10,6 +15,28 @@ function createTwCall(value: types.Expression) {
     )
   }
 
+  if (types.isArrayExpression(value)) {
+    // an element will be null in the case of holes in the array, e.g. [a,,c]
+    // i believe we can just ignore those
+    const elements = value.elements.filter(isTruthy)
+
+    // for now, we won't bother trying to optimize this if there's a spread in the array
+    // it's probably possible to do, but likely rare in practice
+    if (onlyContainsExpressions(elements)) {
+      const constantParts = [
+        types.templateElement({ raw: '' }),
+        ...Array.from({ length: elements.length - 1 }, () => types.templateElement({ raw: ' ' })),
+        types.templateElement({ raw: '' }),
+      ]
+
+      return types.taggedTemplateExpression(
+        types.identifier('tw'),
+        types.templateLiteral(constantParts, elements),
+      )
+    }
+  }
+
+  // for complex calls, pass the expression to the tw function as-is
   return types.callExpression(types.identifier('tw'), [value])
 }
 
